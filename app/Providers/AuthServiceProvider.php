e6f62b7e06d2f3f389f0a64dccba37bf361e68cc
Bruno Brandão Lopes <brandao_lopes@live.com>
2022-03-21 13:06:16 -0300

    incluida traduCao para portugues BR

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Notifications\Notification;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Notifications\Messages\MailMessage;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    VacinApp
                 </div>
 
                 <div class="links">
